ef197330c32880c6cee1eb1a5ab1f0d56287f84b
Matejejko <155886081+Matejejko@users.noreply.github.com>
2024-10-15 15:33:04 +0200

    ZMENA INTEGRITI PRIECINKOV

---
+++ programovanie/12,19.09.py
@@ -0,0 +1,14 @@
+import sys
+x = "hello"
+print ( (x) + " world")
+print (sys.version)
+
+inp = input
+password = input("Create password\n")
+
+
+print (len(password))
+
+if: (password) = 8
+
+
---
+++ programovanie/python uloha 19.09.py
@@ -0,0 +1,14 @@
+print ("Ahoj")
+
+meno = input("Ako sa volas?\n")
+
+print("Ahoj, " + (meno))
+
+priezvisko = input("Tvoje prizvisko?\n")
+
+vek = input("A kolko mas rokov?\n")
+
+print((meno) + " " + (priezvisko) + " " + "mas " + (vek) + " " + "rokov.")
+
+if int(vek) > 40:
+    print("P.S. si starý")
